7e1c010b07e231e521b208a35d7acf75d5454a75
Liam Loxton <liam.loxton@eventbase.com>
2018-10-01 20:28:15 -0400

    Initial Commit

---
+++ Selfigram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Selfigram
+//
+//  Created by Eventbase on 2018-10-01.
+//  Copyright © 2018 Eventbase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
